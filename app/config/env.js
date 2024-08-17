658a3b2832a0b2a73415ef56084f9d377b2055a7
Colocho11 <flopezc18@miumg.edu.gt>
2024-08-17 17:15:20 -0600

    Add files via upload

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'umg_salama_19475',
+  username: 'umg_salama_19475_user',
+  password: 'KMjur7tWeorZgYqUC6LEKkKDaSQ6ujQO',
+  host: 'dpg-cqin53ggph6c738s685g-a',    
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;
